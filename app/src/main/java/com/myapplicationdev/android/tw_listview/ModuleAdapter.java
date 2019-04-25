5929d062f9796b83eb1711e28a81aa4f5a922dd1
lutfiah <16023022@myrp.edu.sg>
2019-04-25 12:53:44 +0800

    Second Activity done

--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,12 +1,57 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
 
+public class ModuleAdapter extends ArrayAdapter<Module> {
+
+    private ArrayList<Module> module;
     private Context context;
+    private TextView tvModuleCode;
+    private ImageView ivProg;
+
+    public ModuleAdapter(Context context, int resource, ArrayList<Module> objects){
+        super(context, resource, objects);
+        // Store the food that is passed to this adapter
+        module = objects;
+        // Store Context object as we would need to use it later
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // The usual way to get the LayoutInflater object to
+        //  "inflate" the XML file into a View object
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // "Inflate" the row.xml as the layout for the View object
+        View rowView = inflater.inflate(R.layout.row_second, parent, false);
+
+        // Get the TextView object
+        tvModuleCode = rowView.findViewById(R.id.tvCode);
+        // Get the ImageView object
+        ivProg = rowView.findViewById(R.id.ivProg);
 
 
+        Module currentModule = module.get(position);
+
+        tvModuleCode.setText(currentModule.getCode());
+        if(currentModule.isProg()) {
+            ivProg.setImageResource(R.drawable.prog);
+        }
+        else {
+            ivProg.setImageResource(R.drawable.nonprog);
+        }
+        // Return the nicely done up View to the ListView
+        return rowView;
+    }
 
 
 }
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,21 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayAdapter aa;
+    ArrayList<Module> module;
 
 
     @Override
@@ -24,8 +32,25 @@ public class SecondActivity extends AppCompatActivity {
         String year = i.getStringExtra("year");
         tvYear.setText(year);
 
+        module = new ArrayList<Module>();
+        module.add(new Module("C208", true));
+        module.add(new Module("B216", false));
+        module.add(new Module("C348", true));
 
+        aa = new ModuleAdapter(this, R.layout.row_second, module);
+        lv.setAdapter(aa);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Module selectedModule = module.get(position);
+
+                Toast.makeText(SecondActivity.this, selectedModule.getCode()
+                                + " Programing: " + selectedModule.isProg(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
