b625b5e800ad4a3056e257573f9680d0b010dc43
haysiA09 <s9907946j>
2019-04-25 12:59:55 +0800

    fcvhv

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -9,18 +9,26 @@ import java.util.ArrayList;
 
 public class FirstActivity extends AppCompatActivity {
 
-    ListView lv;
-    ArrayList<String> al;
-    ArrayAdapter<String> aa;
+
+    ArrayList<Year> year;
+    ArrayAdapter aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView lv=this.findViewById(R.id.lvYear);
 
-        al = new ArrayList<String>();
-        al.add("Year 1");
+        //Create a few year objects in year array
+        year=new ArrayList<Year>();
+        year.add(new Year("Year 1"));
+        year.add(new Year("Year 2"));
+        year.add(new Year("Year 3"));
+
+
+       aa=new YearAdapter(this,R.layout.rowyear,year);
+       lv.setAdapter(aa);
 
 
 
